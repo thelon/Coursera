dd97c7d25ab871b5ed0b910a5226c4230f9d8bbd
Marcin Zawadzki <thelon78@gmail.com>
2013-06-24 21:26:48 +0200

    init array

--- HW1b.js
+++ HW1b.js
@@ -4,22 +4,31 @@ var i;
 var i2;
 var i3;
 var j;
+var j2=0;
 var res=new Array();
-for( i=1;i<=100;i++){
+for( i=1;i<=10000;i++){
   res[i]=1;	
 }
 
-for( i2=2;i2<=10;i2++){
-  for( j=i2*2;j<=100;j=j+i2){
+for( i2=2;i2<=100;i2++){
+  if(res[i2]==0){
+    continue;
+  }
+
+  for( j=i2*2;j<=10000;j=j+i2){
       res[j]=0;
   }
 }
 
 var out = "2";
 
-for ( i3=3;i3<101;i3++){
+for ( i3=3;i3<10001;i3++){
   if (res[i3]>0){
      out = out+","+i3;
+     j2=j2+1;
+  }
+  if (j2>100){
+     break;
   }
   console.log("3 "+i3+"="+res[i3]);
 }
