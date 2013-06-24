6bddbd444388b1dba58efd1deffeb8f791efadcd
Marcin Zawadzki <thelon78@gmail.com>
2013-06-24 20:58:40 +0200

    removing loop

--- HW1b.js
+++ HW1b.js
@@ -1,19 +1,20 @@
 var fs = require('fs');
 var outfile = "HW1b.txt";
-var i=1;
+var i;
+var j;
 var res;
-for(int i=1;i<=100;i++){
+for( i=1;i<=100;i++){
   res[i]=1;	
 }
 
-for(int i=2;i<=10;i++){
-  for(int j=i*2;j<=100;j=j+i){
+for( i=2;i<=10;i++){
+  for( j=i*2;j<=100;j=j+i){
       res[j]=0;
   }
 }
 
 var out = "2";
-for (int i=3;i<101;i++);{
+for ( i=3;i<101;i++);{
   if (res[i]>0){
      out = out+","+i;
   }
